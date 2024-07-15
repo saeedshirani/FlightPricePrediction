fab08beec49f170a10e961019e16ca003b41a4b7
saeed shirani <saeedshirani7878@gmail.com>
2024-07-15 09:33:38 +0330

    Data is filtered.
    Next step Creating regression model

--- Data/main.py
+++ Data/main.py
@@ -10,61 +10,56 @@ class Form:
     def __init__(self):
         self.root = Tk()
         self.root.title("Registration Form")
-        self.root.geometry('300x300')
+        self.root.geometry('500x300')
 
         # Create labels and entry fields for each input
-        self.airline_label = Label(self.root, text="Select Your Prefer Airline (SpiceJet, AirAsia , Vistara, GO_FIRST, Indigo, Air_India) :")
+
+        self.airline_clicked = StringVar()
+        self.airline_clicked.set("Select an Airline")
+        self.airline_label = OptionMenu(self.root, self.airline_clicked, 'SpiceJet', 'AirAsia', 'Vistara', 'GO_FIRST', 'Indigo', 'Air_India')
         self.airline_label.pack()
-        self.airline_entry = Entry(self.root)
-        self.airline_entry.pack()
 
-        self.source_city_label = Label(self.root, text="Enter Your Source City ('Delhi', 'Mumbai', 'Bangalore', 'Kolkata', 'Hyderabad', 'Chennai'): ")
+
+        self.source_city = StringVar()
+        self.source_city.set("Select the source city")
+        self.source_city_label = OptionMenu(self.root, self.source_city, 'Delhi', 'Mumbai', 'Chennai', 'Kolkata', 'Bangalore', 'Hyderabad')
         self.source_city_label.pack()
-        self.source_city_entry = Entry(self.root)
-        self.source_city_entry.pack()
 
-        self.destination_city_label = Label(self.root, text="Enter Your Destination (Mumbai, Bangalore, Kolkata, Hyderabad, Chennai, Delhi):")
+
+        self.destination_city = StringVar()
+        self.destination_city.set("Select the destination city")
+        self.destination_city_label = OptionMenu(self.root, self.destination_city, 'Delhi', 'Mumbai', 'Chennai', 'Kolkata', 'Bangalore', 'Hyderabad')
         self.destination_city_label.pack()
-        self.destination_city_entry = Entry(self.root)
-        self.destination_city_entry.pack()
 
-        self.flight_class_label = Label(self.root, text="Choose your prefer flight class ('Economy', 'Business') :")
+
+        self.flight_class = StringVar()
+        self.flight_class.set("Select the flight class")
+        self.flight_class_label = OptionMenu(self.root, self.flight_class, 'Economy', 'Business')
         self.flight_class_label.pack()
-        self.flight_class_entry = Entry(self.root)
-        self.flight_class_entry.pack()
 
-        self.stops_label = Label(self.root, text="Enter Number of Stops ('zero', 'one', 'two_or_more'): ")
-        self.stops_label.pack()
-        self.stops_entry = Entry(self.root)
-        self.stops_entry.pack()
+        self.stops_count = StringVar()
+        self.stops_count.set("Select the stops count")
+        self.stops_count_label = OptionMenu(self.root, self.stops_count, 'zero', 'one', 'two_or_more')
+        self.stops_count_label.pack()
 
-        self.register_button = Button(self.root, text="Register", command=self.register)
-        self.register_button.pack()
+        self.submit_button = Button(self.root, text="Submit", command=self.register)
+        self.submit_button.pack()
 
     def register(self):
         # Get the user input from the form
-        airline = self.airline_entry.get()
-        source_city = self.source_city_entry.get()
-        destination_city = self.destination_city_entry.get()
-        flight_class = self.flight_class_entry.get()
-        stops = self.stops_entry.get()
+        airline = self.airline_clicked.get()
+        source_city = self.source_city.get()
+        destination_city = self.destination_city.get()
+        flight_class = self.flight_class.get()
+        stops = self.stops_count.get()
 
-        # For now, we will just print the collected data to the console
-        print(f"Airline: {airline}")
-        print(f"Source City: {source_city}")
-        print(f"Destination City: {destination_city}")
-        print(f"Flight Class: {flight_class}")
-        print(f"Stops: {stops}")
-
-        messagebox.showinfo("Success", "Registration successful!")
-        return [airline, source_city, destination_city, flight_class, stops]
-
+        messagebox.showinfo("Success", f"Registration successful!\n{airline}, {source_city}, {destination_city}, {flight_class}, {stops}")
+        self.user_input = [airline, source_city, destination_city, flight_class, stops]
+        self.root.quit()
+        
     def run(self):
         self.root.mainloop()
-
-
-
-
+        return self.user_input
 
 class Data:
 
@@ -72,17 +67,35 @@ class Data:
         self.data_path = './Data/Clean_Dataset.csv'
         self.data = pd.read_csv(self.data_path)
     
-    def filter_data(self, selection_list):
-        airline = 0 
-        sourse_city = 0 
-        destination_city = 0 
-        flight_class = 0
-        stops = 0
+    def filter_data(self):
+       
+        form = Form()
+        user_input = form.run()
 
-        data = 
+        
+        airline, source_city, destination_city, flight_class, stops = user_input
+        print(stops,stops)
 
+        # Placeholder for actual data filtering logic
+        self.filtered_data = self.data.loc[
+            
+            (self.data['airline'] == airline) &
+            (self.data['source_city'] == source_city) &
+            (self.data['destination_city'] == destination_city) & 
+            (self.data['class'] == flight_class) &
+            (self.data['stops'] == stops)
+            
+        ]
+        
 
+        return self.filtered_data
+    
+
+    def linear_model(self):
+        filter_dataset = Data().filter_data()
 
 # Create an instance of the form and run the GUI
-form = Form()
-form.run()
+
+
+data = Data().filter_data()
+print(data.head())

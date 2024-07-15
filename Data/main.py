4919ef1838f7033e19ad1858cc38d5ea963aeb4c
saeed shirani <saeedshirani7878@gmail.com>
2024-07-15 18:01:19 +0330

    Data is filtered now.

--- Data/main.py
+++ Data/main.py
@@ -11,13 +11,10 @@ class Form:
         self.root = Tk()
         self.root.title("Registration Form")
         self.root.geometry('500x300')
+        self.user_input = []
+
 
         # Create labels and entry fields for each input
-
-        self.airline_clicked = StringVar()
-        self.airline_clicked.set("Select an Airline")
-        self.airline_label = OptionMenu(self.root, self.airline_clicked, 'SpiceJet', 'AirAsia', 'Vistara', 'GO_FIRST', 'Indigo', 'Air_India')
-        self.airline_label.pack()
 
 
         self.source_city = StringVar()
@@ -47,55 +44,95 @@ class Form:
 
     def register(self):
         # Get the user input from the form
-        airline = self.airline_clicked.get()
         source_city = self.source_city.get()
         destination_city = self.destination_city.get()
         flight_class = self.flight_class.get()
         stops = self.stops_count.get()
 
-        messagebox.showinfo("Success", f"Registration successful!\n{airline}, {source_city}, {destination_city}, {flight_class}, {stops}")
-        self.user_input = [airline, source_city, destination_city, flight_class, stops]
+        
+        self.user_input = [source_city, destination_city, flight_class, stops]
+        messagebox.showinfo("Success", f"Registration successful!\n {source_city}, {destination_city}, {flight_class}, {stops}")
         self.root.quit()
+
         
     def run(self):
         self.root.mainloop()
         return self.user_input
+    
+        
 
 class Data:
 
-    def __init__(self) -> None:
+    def __init__(self, user_selection) -> None:
         self.data_path = './Data/Clean_Dataset.csv'
         self.data = pd.read_csv(self.data_path)
+        self.user_selection = user_selection
+        self.filtered_data = []
     
     def filter_data(self):
-       
-        form = Form()
-        user_input = form.run()
-
         
-        airline, source_city, destination_city, flight_class, stops = user_input
-        print(stops,stops)
+        source_city, destination_city, flight_class, stops = self.user_selection
 
         # Placeholder for actual data filtering logic
         self.filtered_data = self.data.loc[
-            
-            (self.data['airline'] == airline) &
             (self.data['source_city'] == source_city) &
             (self.data['destination_city'] == destination_city) & 
             (self.data['class'] == flight_class) &
-            (self.data['stops'] == stops)
-            
-        ]
+            (self.data['stops'] == stops)]
         
-
         return self.filtered_data
     
 
-    def linear_model(self):
-        filter_dataset = Data().filter_data()
 
-# Create an instance of the form and run the GUI
+class Report():
+    def __init__(self, selected_data) -> None:
+        self.filtered_data = selected_data
+
+        self.conditional_dict = {}
+        self.stat_dict = {}
+        
+
+    def reporter(self):
+        
+
+        self.unique_airlines = pd.unique(self.filtered_data['airline'])
+
+        
+        for single_airline in self.unique_airlines:
+            self.stat_dict[single_airline] = {
+                                              'mean' : round(self.filtered_data.loc[self.filtered_data['airline'] == single_airline]['price'].mean(),2),
+                                              'mode' : round(self.filtered_data.loc[self.filtered_data['airline'] == single_airline]['price'].mode()[0],2),
+                                              'median': round(self.filtered_data.loc[self.filtered_data['airline'] == single_airline]['price'].median(),2)
+                                                }
+            
+        
+
+        for airliner in self.stat_dict:
 
 
-data = Data().filter_data()
-print(data.head())
+            if self.stat_dict[airliner]['mean'] > self.stat_dict[airliner]['median']:
+                self.conditional_dict[airliner] = (self.stat_dict[airliner]['median'], self.stat_dict[airliner]['mean'] )
+
+            elif  self.stat_dict[airliner]['mean'] < self.stat_dict[airliner]['median']:
+                self.conditional_dict[airliner] = (self.stat_dict[airliner]['mean'], self.stat_dict[airliner]['median'] )
+
+            else:
+                self.conditional_dict[airliner] = (self.stat_dict[airliner]['mode'], self.stat_dict[airliner]['median'] )
+        
+
+
+form = Form().run()
+data = Data(form).filter_data()
+report = Report(data)
+update = report.reporter()
+print(report, report.conditional_dict, report.stat_dict)
+""")
+form = Form().register()
+print(form)
+print(form.user_input)
+data = Data(form.user_input)
+report = Report(data.filtered_data)
+print(report.conditional_dict)
+print(report.stat_dict)
+"""
+
